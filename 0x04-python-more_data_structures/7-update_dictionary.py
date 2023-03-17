3d666dc9a60dd3deb536f5a66f0ece1f554672b4
balogun oluwaseun <vibrator1759@gmail.com>
2023-03-17 15:34:11 -0700

    dictoonary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 
-def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary:
-        del a_dictionary[key]
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
     return (a_dictionary)
